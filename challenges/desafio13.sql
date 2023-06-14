dd6a81078d5e2870bc3e509eb8c2849f2f48301e
muller <pedromullergl11@gmail.com>
2023-06-14 19:10:01 -0300

    feat: query for users age

--- challenges/desafio13.sql
+++ challenges/desafio13.sql
@@ -1,19 +1,14 @@
 SELECT
     CASE
-		  WHEN COUNT(u.user_age) <= 30 THEN 'Até 30 anos'
-		  WHEN COUNT(u.user_age) BETWEEN 31 AND 60 THEN 'Entre 31 e 60 anos'
+		  WHEN u.user_age <= 30 THEN 'Até 30 anos'
+		  WHEN u.user_age BETWEEN 31 AND 60 THEN 'Entre 31 e 60 anos'
 		  ELSE 'Maior de 60 anos'
-        END AS faixa_etaria,
-    CASE
-		  WHEN COUNT(u.user_age) <= 30
-		  WHEN COUNT(u.user_age) BETWEEN 31 AND 60
-		  ELSE COUNT(u.user_age)
-        END AS total_pessoas_usuarias,
+        END AS faixa_etaria,	
+    COUNT(DISTINCT u.user_id) as total_pessoas_usuarias,
+    COUNT(f.user_id) as total_favoritadas
     
 	FROM SpotifyClone.user as u
-    LEFT JOIN SpotifyClone.album as alb ON alb.artist_id = a.artist_id
-	LEFT JOIN SpotifyClone.songs as s ON s.album_id = alb.album_id
-    LEFT JOIN SpotifyClone.favorites as f ON f.song_id = s.song_id
+    LEFT JOIN SpotifyClone.favorites as f ON f.user_id = u.user_id
 	
-    GROUP BY artista
-    ORDER BY COUNT(f.song_id) DESC, artista;
+    GROUP BY faixa_etaria
+    ORDER BY faixa_etaria ASC;
